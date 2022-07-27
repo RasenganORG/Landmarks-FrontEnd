42cc656d65e3b8795de9b105b4043dab395e5e9d
GeorgeFlorian <georgeflorian12@gmail.com>
2022-07-27 15:39:41 +0300

    chore: remove Outlet import from PrivateRoute

--- src/components/Authenticate/PrivateRoute.js
+++ src/components/Authenticate/PrivateRoute.js
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 export default function PrivateRoute({ children }) {
   const location = useLocation();
