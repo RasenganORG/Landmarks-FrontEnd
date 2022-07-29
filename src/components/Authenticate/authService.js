c58d376b19ed1ba9e25cf4c6811bcd9310386c13
GeorgeFlorian <georgeflorian12@gmail.com>
2022-07-29 13:55:37 +0300

    feat: add register-page

--- src/components/Authenticate/authService.js
+++ src/components/Authenticate/authService.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
-const API_USER_URL = 'http://localhost:8080/api/getLoggedUser/';
+const API_GET_LOGGED_USER = 'http://localhost:8080/api/getLoggedUser/';
+const API_REGISTER_USER = 'http://localhost:8080/api/user/';
 
 //register user
 const register = async (userData) => {
-  const response = await axios.post(API_USER_URL, userData, {
+  const response = await axios.post(API_REGISTER_USER, userData, {
     headers: {
       // Overwrite Axios's automatically set Content-Type
       'Content-Type': 'application/json',
@@ -21,7 +22,7 @@ const register = async (userData) => {
 //login user
 const login = async (userData) => {
   const response = await axios.get(
-    `${API_USER_URL}${userData.email}/?pwd=${userData.password}`
+    `${API_GET_LOGGED_USER}${userData.email}/?pwd=${userData.password}`
   );
 
   console.log('login response.data', response.data);
