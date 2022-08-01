0bb2b57966a0b369b953d3dc74781cb46b8937ab
Sloata George <39895513+GeorgeFlorian@users.noreply.github.com>
2022-08-01 15:37:14 +0300

    feat: add register-page (#3)
    * feat: add register-page
    
    * chore: remove console.log() from authService.js

--- src/components/Authenticate/Register.js
+++ src/components/Authenticate/Register.js
@@ -1,5 +1,121 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { authActions, register } from './authSlice';
+
+import {
+  LockOutlined,
+  UserOutlined,
+  MailOutlined,
+  LoadingOutlined,
+} from '@ant-design/icons';
+import { Button, Form, Input, Row, Col, Spin } from 'antd';
+
+const antIcon = (
+  <LoadingOutlined
+    style={{
+      fontSize: 24,
+    }}
+    spin
+  />
+);
 
 export default function Register() {
-  return <div>Register</div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isSuccess, isError, message } = useSelector(
+    (state) => state.auth
+  );
+
+  const onFinish = (values) => {
+    const userData = { ...values.user };
+    console.log('Registered form userData', userData);
+    dispatch(register(userData));
+  };
+
+  useEffect(() => {
+    if (isError) {
+      console.log(message);
+    }
+    if (isSuccess || user) {
+      navigate('/');
+    }
+    dispatch(authActions.reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
+  if (isLoading) return <Spin indicator={antIcon} />;
+
+  return (
+    <Row align='middle' style={{ height: '100vh' }}>
+      <Col span={6} offset={9}>
+        <Form
+          name='register-user'
+          className='login-form'
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name={['user', 'name']}
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Username!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<UserOutlined className='site-form-item-icon' />}
+              placeholder='Username'
+              data-cy='register-username-input'
+            />
+          </Form.Item>
+          <Form.Item
+            name={['user', 'email']}
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Email!',
+                type: 'email',
+              },
+            ]}
+          >
+            <Input
+              prefix={<MailOutlined className='site-form-item-icon' />}
+              placeholder='E-Mail'
+              data-cy='register-email-input'
+            />
+          </Form.Item>
+          <Form.Item
+            name={['user', 'password']}
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className='site-form-item-icon' />}
+              type='password'
+              placeholder='Password'
+              data-cy='register-password-input'
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type='primary'
+              htmlType='submit'
+              className='login-form-button'
+            >
+              Register
+            </Button>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
+  );
 }
--- src/components/Authenticate/authService.js
+++ src/components/Authenticate/authService.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
-const API_USER_URL = 'http://localhost:8080/api/getLoggedUser/';
+const API_GET_LOGGED_USER = 'http://localhost:8080/api/getLoggedUser/';
+const API_REGISTER_USER = 'http://localhost:8080/api/user/';
 
 //register user
 const register = async (userData) => {
-  const response = await axios.post(API_USER_URL, userData, {
+  const response = await axios.post(API_REGISTER_USER, userData, {
     headers: {
       // Overwrite Axios's automatically set Content-Type
       'Content-Type': 'application/json',
     },
   });
 
-  console.log('register response.data', response.data);
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data));
   }
@@ -21,10 +21,8 @@ const register = async (userData) => {
 //login user
 const login = async (userData) => {
   const response = await axios.get(
-    `${API_USER_URL}${userData.email}/?pwd=${userData.password}`
+    `${API_GET_LOGGED_USER}${userData.email}/?pwd=${userData.password}`
   );
-
-  console.log('login response.data', response.data);
 
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data));
