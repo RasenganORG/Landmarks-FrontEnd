3e4f8153e5ed377b66024f9b19c74132697c78c4
GeorgeFlorian <georgeflorian12@gmail.com>
2022-08-02 14:14:13 +0300

    chore: add template for Landing page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,3 +1,35 @@
+import { Typography } from 'antd';
+
+const { Title } = Typography;
+
 export default function Home() {
-  return <div>Lading page with information about how to use the app</div>;
+  return (
+    <div>
+      <Title level={1}>Landmarks</Title>
+      <Title level={2}>Create your next adventure</Title>
+      <Title level={3}>Planning and sharing made easier</Title>
+      <br />
+      <div>
+        <p>
+          With Landmarks you can easily add markers, routes and itineraries of
+          your favourite destinations.
+        </p>
+        <p>The best part ? You can share them with your friends.</p>
+        <p>
+          Everyone can see your planned adventure, but also come up with their
+          own.
+        </p>
+      </div>
+      <br />
+      <div className='card'>
+        <Title level={3}>Create a room</Title>
+        <h1>And start adventuring with your friends.</h1>
+      </div>
+      <br />
+      <div className='card'>
+        <Title level={3}>Join a room</Title>
+        <h1>Get in on the fun.</h1>
+      </div>
+    </div>
+  );
 }
